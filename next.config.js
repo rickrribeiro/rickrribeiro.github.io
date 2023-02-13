8feadf22e69a0e64f4d94ac0d56917fda51ccd55
rickrribeiro <rickribeiro99@gmail.com>
2023-02-13 16:11:49 -0300

    fix: update next config to fix deploy

--- next.config.js
+++ next.config.js
@@ -29,11 +29,22 @@
 
 // module.exports = configs
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true
+//   }
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  }
+  images: {
+    loader: 'akamai',
+    path: ''
+  },
+  assetPrefix: './'
 }
 
-module.exports = nextConfig
+export default nextConfig
