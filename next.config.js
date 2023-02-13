9abb57a6df0d6e8592b8f6b9850f1f8499e8eb8c
rickrribeiro <rickribeiro99@gmail.com>
2023-02-13 16:00:44 -0300

    fix: updating next base path to fix deploy

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
   assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  basePath = `/`
 }
 
 module.exports = {
