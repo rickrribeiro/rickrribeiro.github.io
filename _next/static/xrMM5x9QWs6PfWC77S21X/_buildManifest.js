b9e78d572c428f030bacb573fe990074a8d88a6c
rickrribeiro <rickribeiro99@gmail.com>
2023-03-30 01:36:36 +0000

    Deploying to gh-pages from @ rickrribeiro/rickrribeiro.github.io@67462e526850a1d33a6282d8d27e3197b71c2586 🚀

--- _next/static/xrMM5x9QWs6PfWC77S21X/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-99f892b2931c29df.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
